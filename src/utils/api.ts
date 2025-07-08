c904c4108e69ecf8a6fa9c965585bab2141a93dd
raquis <rizalpratama10010@gmail.com>
2025-07-08 17:37:02 +0700

    feat(prisma): add pagination extension and default config

---
+++ src/utils/api.ts
@@ -0,0 +1,46 @@
+import { CursorPaginationMeta, PageNumberPaginationMeta } from "prisma-extension-pagination";
+import { Response, ResponseCursor, ResponsePagination } from "../interfaces/general/api";
+import { PageNumberCounters } from "prisma-extension-pagination/dist/types";
+import { HonoRequest } from "hono";
+
+interface PaginationParams {
+  page: number;
+  limit: number;
+}
+
+export const createPaginationParams = <T extends string = '/'>(req: HonoRequest<T, {}>): PaginationParams => ({
+  page: (req.query('page') as unknown as number) || 1,
+  limit: (req.query('limit') as unknown as number) || 15,
+})
+
+export const createResponse = <T>(data: T, message: string): Response<T> => ({
+  message,
+  data,
+});
+
+export const createResponsePagination = <T>(data: T, meta: PageNumberPaginationMeta & PageNumberCounters, message: string): ResponsePagination<T> => {
+  return {
+    message,
+    meta: {
+      current_page: meta.currentPage,
+      total_page: meta.pageCount,
+      total_data: meta.totalCount,
+      prev_page: meta.previousPage,
+      next_page: meta.nextPage,
+    },
+    data,
+  };
+}
+
+export const createResponseCursor = <T>(data: T, meta: CursorPaginationMeta, message: string): ResponseCursor<T> => {
+  return {
+    message,
+    meta: {
+      has_prev: meta.hasPreviousPage,
+      has_next: meta.hasNextPage,
+      start: meta.startCursor,
+      end: meta.endCursor,
+    },
+    data,
+  };
+}
